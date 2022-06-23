e56d1e5bc9263a99f70ec780c2ddd7ff362d38e0
rizafdian <afdianriz@gmail.com>
2022-06-23 09:43:18 +0700

    update models

--- application/models/M_laper.php
+++ application/models/M_laper.php
@@ -3,4 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class M_laper extends CI_model
 {
+    public function get_data()
+    {
+        $this->db->select('*');
+        $this->db->from('laporan_perkara');
+        $this->db->order_by('tgl_upload', 'DESC');
+        $query = $this->db->get()->result();
+        return $query;
+    }
 }
